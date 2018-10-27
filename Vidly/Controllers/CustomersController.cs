4e06a03ffc3e6873a7294752ba637de4870d6ed4
Greg <g.j.lane90@gmail.com>
2018-10-27 14:21:10 -0400

    Added customersController, Customers index view, navbar partialview. Customers index displays customers name as links to the customers edit page still being built

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.Viewmodels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+       public List<Customer> customers = new List<Customer>
+        {
+            new Customer{Name = "Greg", Id=1},
+            new Customer{Name = "Kristen", Id=2}
+                
+
+        };
+
+      
+        // GET: Customers
+        public ActionResult Index()
+        {
+         RandomMovieViewModel viewModel = new RandomMovieViewModel
+            {
+                Customers = customers
+            };
+            
+            return View(viewModel);
+            
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = customers.SingleOrDefault(e => e.Id == id);
+
+            if(string.IsNullOrWhiteSpace(customer.Name))
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+
+
+        }
+    }
+}
